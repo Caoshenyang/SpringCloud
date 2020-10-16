777b667a5eb04e463f7c97f2c625ff1fff25eb86
caoshenyang <2417254000@qq.com>
2020-10-16 15:06:14 +0800

    1.基于默认数据库模型的授权与认证

--- spring-security/src/main/java/com/yang/springsecurity/configuration/WebSecurityConfig.java
+++ spring-security/src/main/java/com/yang/springsecurity/configuration/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yang.springsecurity.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,10 +13,16 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,23 +42,48 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
+    /**
+     * 基于默认数据库数据模型用户设置
+     */
     @Bean
-    public UserDetailsService userDetailsService(){
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        //MD5 加密 名文 111 加密后 698d51a19d8a121ce581499d7b701668
-        manager.createUser(User.withUsername("aa").password("{MD5}698d51a19d8a121ce581499d7b701668").roles("USER").build());
-        manager.createUser(User.withUsername("bb").password("{noop}222").roles("USER").build());
+    public UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+        manager.setDataSource(dataSource);
+        if (!manager.userExists("aa")) {
+            //MD5 加密 名文 111 加密后 698d51a19d8a121ce581499d7b701668
+            manager.createUser(User.withUsername("aa").password("{MD5}698d51a19d8a121ce581499d7b701668").roles("USER").build());
 
+        }
+        if (!manager.userExists("bb")) {
+            manager.createUser(User.withUsername("bb").password("{noop}222").roles("USER").build());
+        }
         return manager;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("tom").password("111").roles("ADMIN","USER")
-                .and()
-                .withUser("lisi").password("222").roles("USER");
-    }
+
+//    /**
+//     * 基于内存多用户配置
+//     */
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        //MD5 加密 名文 111 加密后 698d51a19d8a121ce581499d7b701668
+//        manager.createUser(User.withUsername("aa").password("{MD5}698d51a19d8a121ce581499d7b701668").roles("USER").build());
+//        manager.createUser(User.withUsername("bb").password("{noop}222").roles("USER").build());
+//
+//        return manager;
+//    }
+//
+//    /**
+//     * 基于内存多用户配置
+//     */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+//                .withUser("tom").password("111").roles("ADMIN","USER")
+//                .and()
+//                .withUser("lisi").password("222").roles("USER");
+//    }
 
 }
